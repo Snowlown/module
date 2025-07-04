f06dfddb5cb01a63da91044fb756a3655a40a95b
LXB <13068786927@163.com>
2025-07-04 16:45:55 +0800

    fix(app):name age

--- src/HelloRemote.tsx
+++ src/HelloRemote.tsx
@@ -1,3 +1,12 @@
-export default function HelloRemote() {
-  return <div style={{color: 'green',fontSize:'500'}} >我是远程应用的组件 HelloRemote</div>
+export interface IProps {
+    name: string,
+    age: number
+}
+
+export default function HelloRemote({ name, age }: IProps) {
+    return <>
+        <div style={{ color: 'green', fontSize: '500' }} >我是远程应用的组件 HelloRemote</div>
+        <div>name:{name}</div>
+        <div>age:{age}</div>
+    </>
 }
